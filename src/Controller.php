383618f091df994d29d67413a7ff197b6a33933d
Pierre-Hugues Laune <launep@visa.com>
2015-11-10 15:52:10 +0000

    Ajouts POST (controller + service) + Ajout TU pour le service + TODO finir les TU du service, faire les TU pour le controller, modifier la BDD pour faire passer les TU du service

--- src/Controller.php
+++ src/Controller.php
@@ -85,7 +85,7 @@ class Controller extends REST {
         }
         $suppression = $this->service->_deleteUtilisateur($this->_request['id_facebook']);
         $suppression = (int)$this->_request['id'];
-        switch(true){
+        switch (true) {
             case $suppression > 0:
                 $this->response('', 200);
                 break;
@@ -95,6 +95,149 @@ class Controller extends REST {
             default:
                 $this->response('', 500);
                 break;
+        }
+    }
+
+    private function creerGroupe() {
+        if ($this->get_request_method() != "POST") {
+            $this->response('', 406);
+        }
+        $nom = $this->_request['nom'];
+        $admin = $this->_request['admin'];
+        $photo = $this->_request['photo'];
+        if (!empty($nom) && !empty($admin)) {
+            switch ($this->service->_creerGroupe($nom, $admin, $photo)) {
+                case true:
+                    $this->response('', 201);
+                    break;
+                case false:
+                    $this->response('', 400);
+                    break;
+                case -1:
+                    $this->response('Nom de groupe déjà utilisé', 409);
+                    break;
+            }
+        } else {
+            $error = array('status' => "Failed", "msg" => "Invalid json");
+            $this->response($this->json($error), 400);
+        }
+    }
+
+    private function creerCommunaute() {
+        if ($this->get_request_method() != "POST") {
+            $this->response('', 406);
+        }
+        $nom = $this->_request['nom'];
+        $admin = $this->_request['admin'];
+        $photo = $this->_request['photo'];
+        $type = $this->_request['type'];
+        if (!empty($nom) && !empty($admin) && !empty($type)) {
+            switch ($this->service->_creerCommunaute($nom, $admin, $photo, $type)) {
+                case true:
+                    $this->response('', 201);
+                    break;
+                case false:
+                    $this->response('', 400);
+                    break;
+                case -1:
+                    $this->response('Nom de communauté déjà utiliseé', 409);
+                    break;
+            }
+        } else {
+            $error = array('status' => "Failed", "msg" => "Invalid json");
+            $this->response($this->json($error), 400);
+        }
+    }
+
+    private function creerMatch() {
+        if ($this->get_request_method() != "POST") {
+            $this->response('', 406);
+        }
+        $equipe1 = $this->_request['equipe1'];
+        $equipe2 = $this->_request['equipe2'];
+        $date_match = $this->_request['date_match'];
+        if (!empty($equipe1) && !empty($equipe2) && !empty($date_match)) {
+            switch ($this->service->_creerMatch($equipe1, $equipe2, $date_match)) {
+                case true:
+                    $this->response('', 201);
+                    break;
+                case false:
+                    $this->response('', 400);
+                    break;
+            }
+        } else {
+            $error = array('status' => "Failed", "msg" => "Invalid json");
+            $this->response($this->json($error), 400);
+        }
+    }
+
+    private function creerPronostic() {
+        if ($this->get_request_method() != "POST") {
+            $this->response('', 406);
+        }
+        $id_facebook = $this->_request['id_facebook'];
+        $equipe1 = $this->_request['equipe1'];
+        $equipe2 = $this->_request['equipe2'];
+        $date_match = $this->_request['date_match'];
+        $score1 = $this->_request['score1'];
+        $score2 = $this->_request['score2'];
+        $resultat = $this->_request['resultat'];
+        $groupe = $this->_request['groupe'];
+        $communaute = $this->_request['communaute'];
+        if (!empty($id_facebook) && !empty($equipe1) && !empty($equipe2) && !empty($date_match) && !empty($resultat) && (!empty($groupe) || !empty($communaute))) {
+            switch ($this->service->_creerPronostic($id_facebook, $equipe1, $equipe2, $date_match, $score1, $score2, $resultat, $groupe, $communaute)) {
+                case true:
+                    $this->response('', 201);
+                    break;
+                case false:
+                    $this->response('', 400);
+                    break;
+            }
+        } else {
+            $error = array('status' => "Failed", "msg" => "Invalid json");
+            $this->response($this->json($error), 400);
+        }
+    }
+
+    private function ajouterUtilisateurGroupe() {
+        if ($this->get_request_method() != "POST") {
+            $this->response('', 406);
+        }
+        $id_facebook = $this->_request['id_facebook'];
+        $groupe = $this->_request['groupe'];
+        if (!empty($id_facebook) && !empty($groupe)) {
+            switch ($this->service->_ajouterUtilisateurGroupe($id_facebook, $groupe)) {
+                case true:
+                    $this->response('', 201);
+                    break;
+                case false:
+                    $this->response('', 400);
+                    break;
+            }
+        } else {
+            $error = array('status' => "Failed", "msg" => "Invalid json");
+            $this->response($this->json($error), 400);
+        }
+    }
+
+    private function ajouterUtilisateurCommunaute() {
+        if ($this->get_request_method() != "POST") {
+            $this->response('', 406);
+        }
+        $id_facebook = $this->_request['id_facebook'];
+        $communaute = $this->_request['communaute'];
+        if (!empty($id_facebook) && !empty($communaute)) {
+            switch ($this->service->_ajouterUtilisateurCommunaute($id_facebook, $communaute)) {
+                case true:
+                    $this->response('', 201);
+                    break;
+                case false:
+                    $this->response('', 400);
+                    break;
+            }
+        } else {
+            $error = array('status' => "Failed", "msg" => "Invalid json");
+            $this->response($this->json($error), 400);
         }
     }
 
--- src/Service.php
+++ src/Service.php
@@ -28,7 +28,7 @@ class Service {
     }
 
     public function _creerUtilisateur($nom, $prenom, $photo, $id_facebook) {
-        if (empty($id_facebook)) {
+        if (empty($nom) || empty($prenom) || empty($id_facebook)) {
             return false;
         }
         $req = "INSERT INTO Utilisateur(NomUti, PrenomUti, PhotoUti, ID_Facebook) VALUES ('$nom', '$prenom', '$photo', '$id_facebook')";
@@ -64,6 +64,156 @@ class Service {
             return -1;
         }
         return $this->mysqli->affected_rows;
+    }
+
+    public function _creerGroupe($nom, $admin, $photo) {
+        if (empty($nom) || empty($admin) || empty($id_groupe)) {
+            return false;
+        }
+        $req = "SELECT 1 FROM Utilisateur WHERE ID_Facebook='$admin'";
+        if (!($sql = $this->mysqli->query($req))) {
+            error_log($this->mysqli->error);
+            return false;
+        }
+        if($sql->fetch_assoc()[1] != 1){
+            error_log("Impossible de creer le groupe : L'admin n'est pas un utilisateur existant");
+            return false;
+        }
+        $req = "SELECT 1 FROM Groupe WHERE NomGrp='$nom'";
+        if (!($sql = $this->mysqli->query($req))) {
+            error_log($this->mysqli->error);
+            return false;
+        }
+        if($sql->fetch_assoc()[1] != 1){
+            error_log("Impossible de creer le groupe : Le nom a deja ete pris");
+            return -1;
+        }
+
+        $req = "INSERT INTO Groupe(NomGrp, AdminGrp, PhotoGrp, ID_Cla) VALUES ('$nom', '$admin', '$photo', '')";
+        if (!$this->mysqli->query($req)) {
+            error_log($this->mysqli->error);
+            return false;
+        }
+
+        if(!$this->_ajouterUtilisateurGroupe($admin, $nom)){
+            return false;
+        }
+        return true;
+    }
+
+    public function _creerCommunaute($nom, $admin, $photo, $type) {
+        if (empty($nom) || empty($admin) || empty($type)) {
+            return false;
+        }
+        $req = "SELECT 1 FROM Utilisateur WHERE ID_Facebook='$admin'";
+        if (!($sql = $this->mysqli->query($req))) {
+            error_log($this->mysqli->error);
+            return false;
+        }
+        if($sql->fetch_assoc()[1] != 1){
+            error_log("Impossible de creer le groupe : L'admin n'est pas un utilisateur existant");
+            return false;
+        }
+        $req = "SELECT 1 FROM Communaute WHERE NomCom='$nom'";
+        if (!($sql = $this->mysqli->query($req))) {
+            error_log($this->mysqli->error);
+            return false;
+        }
+        if($sql->fetch_assoc()[1] != 1){
+            error_log("Impossible de creer la communaute : Le nom a deja ete pris");
+            return -1;
+        }
+        $req = "INSERT INTO Communaute(NomCom, AdminCom, TypeCom, PhotoCom, ID_Cla) VALUES ('$nom', '$admin', '$type', '$photo', '')";
+        if (!$this->mysqli->query($req)) {
+            error_log($this->mysqli->error);
+            return false;
+        }
+
+        if(!$this->_ajouterUtilisateurCommunaute($admin, $nom)){
+            return false;
+        }
+        return true;
+    }
+
+    public function _creerMatch($equipe1, $equipe2, $date_match) {
+        if (empty($equipe1) || empty($equipe2) || empty($date_match)) {
+            return false;
+        }
+        $req = "SELECT 1 FROM Match_Euro16 WHERE Equipe1='$equipe1' AND Equipe2='$equipe2' AND DateMatch=STR_TO_DATE('$date_match','%d-%m-%Y %h:%i:%s')";
+        if (!($sql = $this->mysqli->query($req))) {
+            error_log($this->mysqli->error);
+            return false;
+        }
+        if($sql->fetch_assoc()[1] == 1){
+            error_log("Impossible de creer le match : Le match est deja present en base");
+            return false;
+        }
+        $req = "INSERT INTO Match_Euro16(Equipe1, Equipe2, Score1, Score2, DateMatch, ID_Mch) VALUES ('$equipe1', '$equipe2', '', '', STR_TO_DATE('$date_match','%d-%m-%Y %h:%i:%s'), '')";
+        if (!$this->mysqli->query($req)) {
+            error_log($this->mysqli->error);
+            return false;
+        }
+        return true;
+    }
+
+    public function _creerPronostic($id_facebook, $equipe1, $equipe2, $date_match, $score1, $score2, $resultat, $groupe, $communaute) {
+        if (empty($id_facebook) || empty($equipe1) || empty($equipe2) || empty($date_match) || empty($resultat)) {
+            return false;
+        }
+        if (!empty($groupe)) {
+            $req = "SELECT ID_Cla FROM Groupe WHERE NomGrp='$groupe'";
+            if (!($sql = $this->mysqli->query($req))) {
+                error_log($this->mysqli->error);
+                return false;
+            }
+            $ID_Cla = $sql->fetch_assoc()[1];
+        } else if (!empty($communaute)) {
+            $req = "SELECT ID_Cla FROM Communaute WHERE NomCom='$communaute'";
+            if (!($sql = $this->mysqli->query($req))) {
+                error_log($this->mysqli->error);
+                return false;
+            }
+            $ID_Cla = $sql->fetch_assoc()[1];
+        } else if (empty($groupe) && empty($communaute)){
+            $ID_Cla = "ID_GLOBAL";
+        }
+        $req = "SELECT ID_Mch FROM Match_Euro16 WHERE Equipe1='$equipe1' AND Equipe2='$equipe2' AND DateMatch='$date_match'";
+        if (!($sql = $this->mysqli->query($req))) {
+            error_log($this->mysqli->error);
+            return false;
+        }
+        $ID_Mch = $sql->fetch_assoc()[1];
+
+        $req = "INSERT INTO Pronostic(Score1, Score2, ID_Mch, Resultat, ID_Cla) VALUES ('$score1', '$score2', '$ID_Mch', '$resultat', '$ID_Cla')";
+        if (!$this->mysqli->query($req)) {
+            error_log($this->mysqli->error);
+            return false;
+        }
+        return true;
+    }
+
+    public function _ajouterUtilisateurGroupe($id_facebook, $groupe) {
+        if (empty($id_facebook) || empty($groupe)) {
+            return false;
+        }
+        $req = "INSERT INTO Utilisateur_Groupe(Utilisateur, Groupe) VALUES ('$id_facebook', '$groupe')";
+        if (!$this->mysqli->query($req)) {
+            error_log($this->mysqli->error);
+            return false;
+        }
+        return true;
+    }
+
+    public function _ajouterUtilisateurCommunaute($id_facebook, $communaute) {
+        if (empty($id_facebook) || empty($communaute)) {
+            return false;
+        }
+        $req = "INSERT INTO Utilisateur_Communaute(Utilisateur, Communaute) VALUES ('$id_facebook', '$communaute')";
+        if (!$this->mysqli->query($req)) {
+            error_log($this->mysqli->error);
+            return false;
+        }
+        return true;
     }
 }
 
