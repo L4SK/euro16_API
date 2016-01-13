d46c094e6ac93d3abf2cd764dd43ad08c92ba494
GuillaumeRot <guillaume.rot@gmail.com>
2016-01-13 19:25:47 +0100

    End points post + get fonctionnel

--- tests/integration/ControllerEndpointsTest.php
+++ tests/integration/ControllerEndpointsTest.php
@@ -3,7 +3,7 @@
 require_once('../../vendor/autoload.php');
 require_once('../../config.php');
 
-$GLOBALS['api_url'] = 'http://localhost:63342/euro16_API/index.php?rquest=';
+$GLOBALS['api_url'] = 'http://localhost/euro16_API/index.php?rquest=';
 
 class ControllerEndpointsTest extends PHPUnit_Framework_TestCase {
 
@@ -20,9 +20,43 @@ class ControllerEndpointsTest extends PHPUnit_Framework_TestCase {
      * Methodes utilisables pour executer les requetes disponibles ici (Rubrique : "Requests Methods") : https://guzzle.readthedocs.org/en/guzzle4/http-messages.html
      */
 
+    public function testEndpointCreerUtilisateurSuccess() {
+        $nom = "NomTest";
+        $prenom = "PrenomTest";
+        $photo = "PhotoTest";
+        $id_facebook = "IdFacebookTest";
+        $data = array(
+            "nom" => $nom,
+            "prenom" => $prenom,
+            "photo" => $photo,
+            "id_facebook" => $id_facebook
+        );
+        $requete = $this->client->post($GLOBALS['api_url'] . 'creerUtilisateur&cle=' . $GLOBALS["cle"],
+            array(
+                "json" => $data
+            )
+        );
+        $this->assertEquals(201, $requete->getStatusCode());
+    }
+
     public function testEndpointGetUtilisateursSuccess() {
+        $nom = "NomTest";
+        $prenom = "PrenomTest";
+        $photo = "PhotoTest";
+        $id_facebook = "IdFacebookTest";
+        $this->client->post($GLOBALS['api_url'] . 'creerUtilisateur&cle=' . $GLOBALS["cle"],
+            array(
+                "json" => array(
+                    "nom" => $nom,
+                    "prenom" => $prenom,
+                    "photo" => $photo,
+                    "id_facebook" => $id_facebook
+                )
+            )
+        );
         $requete = $this->client->get($GLOBALS['api_url'] . 'getUtilisateurs&cle=' . $GLOBALS["cle"]);
         $this->assertEquals(200, $requete->getStatusCode());
+        $this->assertEquals(json_encode(array(array("NomUti" => $nom, "PrenomUti" => $prenom, "PhotoUti" => $photo, "ID_Facebook" => $id_facebook))), (string)$requete->getBody());
     }
 }
 
